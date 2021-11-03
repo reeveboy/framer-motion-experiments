bc7e00f594a86326872c251d81d43f2247112f89
reeveboy <reeveboy10@gmail.com>
2021-11-03 12:24:22 +0530

    Loading Component

---
+++ components/Loading.js
@@ -0,0 +1,47 @@
+import React from "react";
+import { motion } from "framer-motion";
+
+export default function Loading() {
+  const blockVariants = {
+    start: {
+      x: -200,
+      rotate: 0,
+    },
+    target: {
+      x: 200,
+      rotate: 270,
+    },
+  };
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        width: "100%",
+        height: "100vh",
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#FFCCFF",
+      }}>
+      <motion.div
+        style={{
+          borderRadius: "20px",
+          width: "150px",
+          height: "150px",
+          background: "linear-gradient(90deg,#ffa0ae 0%,#aacaef 75%)",
+        }}
+        initial="start"
+        animate="target"
+        transition={{
+          type: "tween",
+          ease: "easeInOut",
+          repeat: Infinity,
+          repeatType: "reverse",
+          repeatDelay: 1,
+          duration: 1,
+        }}
+        variants={blockVariants}
+      />
+    </div>
+  );
+}
